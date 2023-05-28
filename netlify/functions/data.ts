fa5662e57519de7c996155db3f4b9c35c4c81cd0
Maxime Laigre <maxime.laigre@gmail.com>
2023-05-28 15:32:16 +0200

    debug

--- netlify/functions/data.ts
+++ netlify/functions/data.ts
@@ -3,7 +3,9 @@ import { db } from "./db";
 
 export const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
     const collection = await db()
-    const { color } = await collection.findOne()
+    const value = await collection.findOne()
+    console.log(value)
+    const { color } = value
     return {
         statusCode: 200,
         body: JSON.stringify({ color }),
